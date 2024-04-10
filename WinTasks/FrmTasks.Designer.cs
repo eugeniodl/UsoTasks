c10a893092cd663117166c2961ab89b4d8713382
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-04-10 16:42:08 -0600

    Agregar archivos de proyecto.

---
+++ WinTasks/FrmTasks.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WinTasks
+{
+    partial class FrmTasks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.pb1 = new System.Windows.Forms.ProgressBar();
+            this.pb2 = new System.Windows.Forms.ProgressBar();
+            this.pb3 = new System.Windows.Forms.ProgressBar();
+            this.pb4 = new System.Windows.Forms.ProgressBar();
+            this.txtConteo1 = new System.Windows.Forms.TextBox();
+            this.txtConteo2 = new System.Windows.Forms.TextBox();
+            this.txtConteo3 = new System.Windows.Forms.TextBox();
+            this.txtConteo4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(25, 22);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(26, 64);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(74, 23);
+            this.pb1.TabIndex = 1;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(115, 64);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(74, 23);
+            this.pb2.TabIndex = 2;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(204, 64);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(74, 23);
+            this.pb3.TabIndex = 3;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(300, 64);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(74, 23);
+            this.pb4.TabIndex = 4;
+            // 
+            // txtConteo1
+            // 
+            this.txtConteo1.Location = new System.Drawing.Point(26, 107);
+            this.txtConteo1.Multiline = true;
+            this.txtConteo1.Name = "txtConteo1";
+            this.txtConteo1.Size = new System.Drawing.Size(74, 230);
+            this.txtConteo1.TabIndex = 5;
+            // 
+            // txtConteo2
+            // 
+            this.txtConteo2.Location = new System.Drawing.Point(115, 107);
+            this.txtConteo2.Multiline = true;
+            this.txtConteo2.Name = "txtConteo2";
+            this.txtConteo2.Size = new System.Drawing.Size(74, 230);
+            this.txtConteo2.TabIndex = 6;
+            // 
+            // txtConteo3
+            // 
+            this.txtConteo3.Location = new System.Drawing.Point(204, 107);
+            this.txtConteo3.Multiline = true;
+            this.txtConteo3.Name = "txtConteo3";
+            this.txtConteo3.Size = new System.Drawing.Size(74, 230);
+            this.txtConteo3.TabIndex = 7;
+            // 
+            // txtConteo4
+            // 
+            this.txtConteo4.Location = new System.Drawing.Point(300, 107);
+            this.txtConteo4.Multiline = true;
+            this.txtConteo4.Name = "txtConteo4";
+            this.txtConteo4.Size = new System.Drawing.Size(74, 230);
+            this.txtConteo4.TabIndex = 8;
+            // 
+            // FrmTasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 366);
+            this.Controls.Add(this.txtConteo4);
+            this.Controls.Add(this.txtConteo3);
+            this.Controls.Add(this.txtConteo2);
+            this.Controls.Add(this.txtConteo1);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "FrmTasks";
+            this.Text = "FrmTasks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnIniciar;
+        private ProgressBar pb1;
+        private ProgressBar pb2;
+        private ProgressBar pb3;
+        private ProgressBar pb4;
+        private TextBox txtConteo1;
+        private TextBox txtConteo2;
+        private TextBox txtConteo3;
+        private TextBox txtConteo4;
+    }
+}
---
+++ WinTasks/FrmTasks.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTasks
+{
+    public partial class FrmTasks : Form
+    {
+        private TaskScheduler _scheduler;
+        public FrmTasks()
+        {
+            InitializeComponent();
+            _scheduler
+                = TaskScheduler.FromCurrentSynchronizationContext();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            new Task(() =>
+            {
+                Task[] tareas =
+                {
+                    Task.Factory.StartNew(() =>
+                IncreaseValue(txtConteo1, pb1, 150, 1),
+                CancellationToken.None,
+                TaskCreationOptions.LongRunning,
+                _scheduler),
+                Task.Factory.StartNew(() =>
+                IncreaseValue(txtConteo2, pb2, 180, 1),
+                CancellationToken.None,
+                TaskCreationOptions.LongRunning,
+                _scheduler),
+                Task.Factory.StartNew(() =>
+                IncreaseValue(txtConteo3, pb3, 50, 1),
+                CancellationToken.None,
+                TaskCreationOptions.LongRunning,
+                _scheduler),
+                Task.Factory.StartNew(() =>
+                IncreaseValue(txtConteo4, pb4, 200, 1),
+                CancellationToken.None,
+                TaskCreationOptions.LongRunning,
+                _scheduler)
+                };
+                Task.WaitAll(tareas);
+            }).Start();
+            MessageBox.Show("Se han terminado todas las tareas");
+        }
+
+        private void IncreaseValue(TextBox t, ProgressBar p, int r, int c)
+        {
+            p.Maximum = r;
+            for (int i = 0; i < r; i++)
+            {
+                int result = i * c;
+                Thread.Sleep(100);
+                UpdateView(t, p, result);
+            }
+        }
+
+        private void UpdateView(TextBox t, ProgressBar p, int result)
+        {
+            p.Value = result;
+            t.Text += "Conteo: " + result + Environment.NewLine;
+            t.SelectionStart = t.Text.Length;
+            t.ScrollToCaret();
+            Application.DoEvents();
+        }
+    }
+}
